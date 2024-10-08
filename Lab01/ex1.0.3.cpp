7037ed378a134c21ba35a49e73bd37fa516e5d11
bachhungcb <bachhungcb@gmail.com>
2024-10-08 20:10:49 +0700

    solve problem 0.2 and 0.3 on hustack

---
+++ Lab01/ex1.0.3.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+const int N = 1100;
+
+int a[N][N];
+int n,m;
+int ans;
+int h[N];
+stack<int> S;
+vector<int> V;
+int L[N], R[N];
+
+void input(){
+    cin >> n >> m;
+    for (int i = 1; i<= n; i++){
+        for(int j = 1; j <= m; j++){
+            cin >> a[i][j];
+        }
+    }
+}
+
+int compute(){
+    h[0] = -1;
+    h[m+1] = -1;
+    V.clear();
+
+    for(int i = 1; i <= m+1; i++){
+        while(!V.empty() && h[i] < h[V[V.size() -1]]){
+            R[V[V.size() - 1]] = i;
+            V.pop_back();
+        }
+        V.push_back(i);
+    }
+
+    for(int i = m; i >= 0; i--){
+        while(!V.empty() && h[i] < h[V[V.size() -1]]){
+            L[V[V.size() - 1]] = i;
+            V.pop_back();
+        }
+        V.push_back(i);
+    }
+
+    int ans = 0;
+    for(int i = 1; i <= m; i ++){
+        int c = (R[i] - L[i] -1 ) * h[i];
+        ans = ans < c ? c : ans;
+    }
+
+    return ans;
+
+}
+
+void solve(){
+    int ans = 0;
+    for(int i = 1; i <= m; i++){
+        h[i] = 0;
+    }
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(a[i][j] == 0){
+                h[j] = 0;
+            }else{
+                h[j] += 1;
+            }
+        }
+        int t = compute();
+        if(t > ans)
+            ans = t;
+    }
+    cout << ans;
+}
+
+
+int main(){
+    input();
+    solve();
+    return 0;
+}
